1ca0093bf555211574a1cfd4bbae0664fdf5c266
Quentin Boonekeh <nombohq14@gmail.com>
2022-09-15 22:47:29 +0900

    started Sell page

--- src/pages/sell/Sell.jsx
+++ src/pages/sell/Sell.jsx
@@ -1,9 +1,22 @@
 import "./sell.css";
 
+import Upbar from "../../components/upbar/Upbar";
+import Footer from "../../components/footer/Footer";
+import MMenu from "../../mobile/mmenu/Mmenu";
+
 import React from "react";
+import Mtop from "../../mobile/mUpbar/Mtop";
 
 function Sell() {
-  return <div>Sell</div>;
+  return (
+    <div className="container">
+      <Upbar />
+      <Mtop />
+      <MMenu />
+      <div className="sell_container"></div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Sell;
