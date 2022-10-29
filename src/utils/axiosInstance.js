b4e9425e8c1d6cd87fa8848dfb68f4fd8469afb3
Nomboh Quentin <nombohq14@gmail.com>
2022-10-30 01:57:45 +0900

    route

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = () =>
   axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://brokang-api.herokuapp.com/api/v1",
     withCredentials: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
